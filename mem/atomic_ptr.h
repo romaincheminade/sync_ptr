0fc3430bf8f3dce4b774e191e2e8a95471e09b1f
Romain Cheminade <designedbyaleister@free.fr>
2017-07-02 01:01:24 +0100

    atomic_ptr wip

--- mem/atomic_ptr.h
+++ mem/atomic_ptr.h
@@ -6,6 +6,7 @@
 #include <atomic>
 #include <cassert>
 #include <cstdint>
+#include <type_traits>
 #include <utility>
 
 #ifndef __MEM_ALLOCATION_POLICY_H__
@@ -15,10 +16,36 @@
 
 namespace mem
 {
+    template <typename T>
+    inline void _store_release(volatile T * p_dest, T p_value)
+    {
+        std::atomic_thread_fence(std::memory_order_release);
+        *p_dest = p_value;
+    }
+
+    template<typename T>
+    inline T _load_acquire(volatile T * p_src)
+    {
+        T res = *p_src;
+        std::atomic_thread_fence(std::memory_order_acquire);
+        return res;
+    }
+
+    template <typename T>
+    inline T _exchange_acquire_release(volatile T * p_dest, T p_value)
+    {
+        T res = _load_acquire(p_dest);
+        _store_release(p_dest, p_value);
+        return res;
+    }
+
+
+    template<class TPtr>
+    using atomic_ptr_deleter = default_deleter<TPtr>;
 
     template <
         class TPtr,
-        template <class T> class TDeleter = default_deleter<TPtr>>
+        template <class T> class TDeleter = atomic_ptr_deleter>
     class atomic_ptr final
         : private TDeleter<TPtr>
     {
@@ -27,19 +54,20 @@ namespace mem
         using atomic_ptr_t  = atomic_ptr<TPtr, TDeleter>;
 
     private:
-        std::atomic<TPtr *>     atomic_ptr_;
+        TPtr *      ptr_;
 
 
     public:
         atomic_ptr(void) noexcept
-            : atomic_ptr_{ nullptr }
+            : ptr_{ nullptr }
         {}
 
         template<class TPtrCompatible>
         atomic_ptr(TPtrCompatible * p_ptr) noexcept
-            : atomic_ptr_{ p_ptr }
+            : ptr_{ nullptr }
         {
             assert(p_ptr);
+            _store_release(&ptr_, p_ptr);
         }
 
         ~atomic_ptr(void) noexcept
@@ -50,12 +78,14 @@ namespace mem
 
     public:
         atomic_ptr(atomic_ptr_t && p_rhs) noexcept
-            : atomic_ptr_{ p_rhs.get() }
-        {}
+            : ptr_{ nullptr }
+        {
+            _store_release(&ptr_, p_rhs.get());
+        }
 
         atomic_ptr_t & operator=(atomic_ptr_t && p_rhs) noexcept
         {
-            atomic_ptr_.store(p_rhs.get(), std::memory_order_release);
+            _store_release(&ptr_, p_rhs.get());
             return *this;
         }
 
@@ -66,9 +96,7 @@ namespace mem
     private:
         TPtr * set(TPtr * p_ptr) noexcept
         {
-            auto p = get();
-            atomic_ptr_.store(p_ptr, std::memory_order_release);
-            return p;
+            return _exchange_acquire_release(&ptr_, p_ptr);
         }
 
         void release_ptr(TPtr * p_ptr) noexcept
@@ -82,15 +110,6 @@ namespace mem
             {
                 this->deallocate(p);
             }
-        }
-
-
-    public:
-        void swap(atomic_ptr_t & p_rhs) noexcept
-        {
-            assert(0);
-            //atomic_ptr_.store(p_rhs.get(), std::memory_order_release);
-            //p_rhs.atomic_ptr_.store(get(), std::memory_order_release);
         }
 
 
@@ -123,9 +142,14 @@ namespace mem
 
 
     public:
+        TPtr * non_atomic_get(void) const noexcept
+        {
+            return ptr_;
+        }
+
         TPtr * get(void) const noexcept
         {
-            return atomic_ptr_.load(std::memory_order_acquire);
+            return _load_acquire(&ptr_);
         }
 
         TPtr * operator->(void) const noexcept
@@ -161,7 +185,7 @@ namespace mem
 
     template <
         class TPtr,
-        template <class T> class TDeleter = sync_ptr_deleter,
+        template <class T> class TDeleter = mem::atomic_ptr_deleter,
         class... TArgs>
     inline typename std::enable_if<
         !std::is_array<TPtr>::value,
@@ -186,7 +210,7 @@ namespace mem
     template <
         class TPtr,
         template <class T> class TAllocator,
-        template <class T> class TDeleter = sync_ptr_deleter,
+        template <class T> class TDeleter = mem::atomic_ptr_deleter,
         class... TArgs>
     inline typename std::enable_if<
         !std::is_array<TPtr>::value,
@@ -214,20 +238,20 @@ namespace mem
 
 //=============================================================================
 
-namespace std
-{
-    
-    template <
-        class TPtr,
-        template <class T> class TDeleter>
-    void swap(
-        mem::atomic_ptr<TPtr, TDeleter> & p_lhs,
-        mem::atomic_ptr<TPtr, TDeleter> & p_rhs)
-        noexcept
-    {
-        p_lhs.swap(p_rhs);
-    }
-
-} // namespace std
+//namespace std
+//{
+//
+//    template <
+//        class TPtr,
+//        template <class T> class TDeleter>
+//    void swap(
+//        mem::atomic_ptr<TPtr, TDeleter> & p_lhs,
+//        mem::atomic_ptr<TPtr, TDeleter> & p_rhs)
+//        noexcept
+//    {
+//        p_lhs.swap(p_rhs);
+//    }
+//
+//} // namespace std
 
 #endif // __MEM_ATOMIC_PTR_H__
--- test/sync_ptr/main.cpp
+++ test/sync_ptr/main.cpp
@@ -1,6 +1,7 @@
 
 #include "mem_sync_ptr.h"
 
+#include <mem/atomic_ptr.h>
 #include <mem/single_ptr.h>
 
 #include <cstdio>
@@ -18,6 +19,11 @@ int main(
 
 
     class Obj {};
+
+    mem::atomic_ptr<Obj> ao;
+    std::cout << std::size_t(ao.get()) << std::endl;
+    std::cout << std::size_t(ao.non_atomic_get()) << std::endl;
+
     auto o = mem::single_ptr<Obj>::instance();
     auto s = sizeof(o);
     std::cout << s << std::endl;
